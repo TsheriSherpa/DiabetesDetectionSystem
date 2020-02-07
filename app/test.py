2f71b9771399582bc507d818c78e1e19352bd76e
Chiree Sherpa <chiree.sherpa@newitventure.com>
2020-02-07 17:42:28 +0545

    changes

--- app/test.py
+++ app/test.py
@@ -66,6 +66,30 @@ def checkUsingNaiveBayes(base_dir, data:list):
     print('The result is ' + result)
     return 'Positive' if result == '1' else 'Negative'
 
+
+def checkUsingDT(base_dir, data:list):
+    import pickle
+    import pandas as pd
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.model_selection import train_test_split
+    
+    
+    diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv') 
+    tree = DecisionTreeClassifier(max_depth=3, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+                                                diabetes.loc[:, diabetes.columns != 'Outcome'],
+                                                diabetes['Outcome'], stratify=diabetes['Outcome'],
+                                                random_state=66)
+    tree.fit(X_train, y_train)
+    # pickle.dump(naiveBayes, open(base_dir + '/../diabetesModel/decisionTreeModel','wb'))
+    loaded_model = pickle.load(open(base_dir + '/../diabetesModel/decisionTreeModel', 'rb'))
+    print("model imported")
+    # result = loaded_model.score(X_test,y_test)
+    result = str((loaded_model.predict([data]))[0])
+    print('The result is ' + result)
+    return 'Positive' if result == '1' else 'Negative'
+    
+    
 # def importModules():
 #     import pickle
 #     import pandas as pd
