b654a09ff402d0089dee5f68157b727e5e6b9122
tsheri <tsherisherpa@gmail.com>
2020-03-02 22:10:48 +0545

    changes

--- app/configuration.py
+++ app/configuration.py
@@ -9,5 +9,5 @@ class Config():
 	SECRET_KEY   = "77tgFCdrEEdv77554##@3" 
 	
 	SQLALCHEMY_TRACK_MODIFICATIONS 	= False
-
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+	
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/flask_db'
