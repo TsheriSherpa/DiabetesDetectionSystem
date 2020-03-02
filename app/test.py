d2e4dd01e38739e6a7fe1bb6f335fc32a7030bcd
Chiree Sherpa <chiree.sherpa@newitventure.com>
2020-03-02 13:08:59 +0545

    imporved decision tree acurracy

--- app/test.py
+++ app/test.py
@@ -29,12 +29,13 @@ def checkUsingKNN(base_dir, data:list):
     #     test_accuracy.append(knn.score(X_test, y_test))
     # knn = KNeighborsClassifier(n_neighbors=9)   # neightbour 9 has best accuracy
     # knn.fit(X_train, y_train)
-    #print('Exporting model.')
+    # #print('Exporting model.')
     # pickle.dump(knn, open(base_dir+'/../diabetesModel/knnModel','wb'))
     
     loaded_model = pickle.load(open(base_dir+'/../diabetesModel/knnModel', 'rb'))
     print("model imported")
     result = str((loaded_model.predict([data]))[0])
+    # result = str((knn.predict([data]))[0])
 
     print('The result for KNN is '+str(result))
     return 'Positive' if result == '1' else 'Negative'
@@ -71,33 +72,26 @@ def checkUsingNaiveBayes(base_dir, data:list):
 
 def checkUsingDT(base_dir, data:list):
     import pickle
-    import pandas as pd
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.model_selection import train_test_split
+    # import pandas as pd
+    # from sklearn.tree import DecisionTreeClassifier
+    # from sklearn.model_selection import train_test_split
+    # from sklearn.metrics import accuracy_score
     
     
     # diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv') 
-    # tree = DecisionTreeClassifier(max_depth=3, random_state=66)
+    # tree = DecisionTreeClassifier(max_depth=3, random_state=0)
     # X_train, X_test, y_train, y_test = train_test_split(
     #                                             diabetes.loc[:, diabetes.columns != 'Outcome'],
     #                                             diabetes['Outcome'], stratify=diabetes['Outcome'],
-    #                                             random_state=66)
+    #                                             random_state=0)
     # tree.fit(X_train, y_train)
     # pickle.dump(tree, open(base_dir + '/../diabetesModel/decisionTreeModel','wb'))
 
     loaded_model = pickle.load(open(base_dir + '/../diabetesModel/decisionTreeModel', 'rb'))
     print("model imported")
+    # pred = tree.predict(X_test)
+    # print("Score is: "+str(accuracy_score(y_test, pred)))
     result = str((loaded_model.predict([data]))[0])
-
+    # result = str((tree.predict([data]))[0])
     print('The result for DT is ' + result)
     return 'Positive' if result == '1' else 'Negative'
-
-    
-# def importModules():
-#     import pickle
-#     import pandas as pd
-#     from sklearn.naive_bayes import GaussianNB 
-#     from sklearn.metrics import accuracy_score 
-#     from sklearn.preprocessing import StandardScaler
-#     from sklearn.neighbors import KNeighborsClassifier  
-#     from sklearn.model_selection import train_test_split
