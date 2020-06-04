3a0c9884cb4f3576553d25385863f899164a9f24
Tsheri Sherpa <tsherisherpa@gmail.com>
2020-06-04 09:58:18 +0545

    changes

--- app/test.py
+++ app/test.py
@@ -1,36 +1,36 @@
 
 def checkUsingKNN(base_dir, data:list):
     import pickle
-    # import pandas as pd
-    # from sklearn.model_selection import train_test_split
-    # from sklearn.neighbors import KNeighborsClassifier 
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.neighbors import KNeighborsClassifier 
     
     
-    # diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv')   #importing dataset.
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     diabetes.loc[:, diabetes.columns != 'Outcome'], 
-    #     diabetes['Outcome'], 
-    #     stratify=diabetes['Outcome'],
-    #     random_state=66
-    # )
+    diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv')   #importing dataset.
+    X_train, X_test, y_train, y_test = train_test_split(
+        diabetes.loc[:, diabetes.columns != 'Outcome'], 
+        diabetes['Outcome'], 
+        stratify=diabetes['Outcome'],
+        random_state=66
+    )
 
-    # training_accuracy = []
-    # test_accuracy = []
-    # # try n_neighbors from 1 to 10
-    # neighbors_settings = range(1, 11)
+    training_accuracy = []
+    test_accuracy = []
+    # try n_neighbors from 1 to 10
+    neighbors_settings = range(1, 11)
 
-    # for n_neighbors in neighbors_settings:  #finding the best neighbour for knn model.
-    #     # build the model
-    #     knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-    #     knn.fit(X_train, y_train)
-    #     # record training set accuracy
-    #     training_accuracy.append(knn.score(X_train, y_train))
-    #     # record test set accuracy
-    #     test_accuracy.append(knn.score(X_test, y_test))
-    # knn = KNeighborsClassifier(n_neighbors=9)   # neightbour 9 has best accuracy
-    # knn.fit(X_train, y_train)
-    #print('Exporting model.')
-    # pickle.dump(knn, open(base_dir+'/../diabetesModel/knnModel','wb'))
+    for n_neighbors in neighbors_settings:  #finding the best neighbour for knn model.
+        # build the model
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(X_train, y_train)
+        # record training set accuracy
+        training_accuracy.append(knn.score(X_train, y_train))
+        # record test set accuracy
+        test_accuracy.append(knn.score(X_test, y_test))
+    knn = KNeighborsClassifier(n_neighbors=9)   # neightbour 9 has best accuracy
+    knn.fit(X_train, y_train)
+    print('Exporting model.')
+    pickle.dump(knn, open(base_dir+'/../diabetesModel/knnModel','wb'))
     
     loaded_model = pickle.load(open(base_dir+'/../diabetesModel/knnModel', 'rb'))
     print("model imported")
@@ -43,23 +43,23 @@ def checkUsingKNN(base_dir, data:list):
 
 def checkUsingNaiveBayes(base_dir, data:list):
     import pickle
-    # import pandas as pd
-    # from sklearn.naive_bayes import GaussianNB 
-    # from sklearn.metrics import accuracy_score 
-    # from sklearn.preprocessing import StandardScaler
-    # from sklearn.model_selection import train_test_split
+    import pandas as pd
+    from sklearn.naive_bayes import GaussianNB 
+    from sklearn.metrics import accuracy_score 
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
     
     
-    # diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv')   #importing dataset.
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     diabetes.loc[:, diabetes.columns != 'Outcome'], 
-    #     diabetes['Outcome'], 
-    #     stratify=diabetes['Outcome'],
-    #     random_state=66
-    # )
-    # naiveBayes = GaussianNB()
-    # naiveBayes.fit(X_train, y_train)
-    # pickle.dump(naiveBayes, open(base_dir + '/../diabetesModel/naiveBayesModel','wb'))
+    diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv')   #importing dataset.
+    X_train, X_test, y_train, y_test = train_test_split(
+        diabetes.loc[:, diabetes.columns != 'Outcome'], 
+        diabetes['Outcome'], 
+        stratify=diabetes['Outcome'],
+        random_state=66
+    )
+    naiveBayes = GaussianNB()
+    naiveBayes.fit(X_train, y_train)
+    pickle.dump(naiveBayes, open(base_dir + '/../diabetesModel/naiveBayesModel','wb'))
 
     loaded_model = pickle.load(open(base_dir + '/../diabetesModel/naiveBayesModel', 'rb'))
     print("model imported")
@@ -76,14 +76,14 @@ def checkUsingDT(base_dir, data:list):
     from sklearn.model_selection import train_test_split
     
     
-    # diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv') 
-    # tree = DecisionTreeClassifier(max_depth=3, random_state=66)
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #                                             diabetes.loc[:, diabetes.columns != 'Outcome'],
-    #                                             diabetes['Outcome'], stratify=diabetes['Outcome'],
-    #                                             random_state=66)
-    # tree.fit(X_train, y_train)
-    # pickle.dump(tree, open(base_dir + '/../diabetesModel/decisionTreeModel','wb'))
+    diabetes = pd.read_csv(base_dir+'/../diabetesModel/diabetes.csv') 
+    tree = DecisionTreeClassifier(max_depth=3, random_state=66)
+    X_train, X_test, y_train, y_test = train_test_split(
+                                                diabetes.loc[:, diabetes.columns != 'Outcome'],
+                                                diabetes['Outcome'], stratify=diabetes['Outcome'],
+                                                random_state=66)
+    tree.fit(X_train, y_train)
+    pickle.dump(tree, open(base_dir + '/../diabetesModel/decisionTreeModel','wb'))
 
     loaded_model = pickle.load(open(base_dir + '/../diabetesModel/decisionTreeModel', 'rb'))
     print("model imported")
