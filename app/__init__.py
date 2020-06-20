19938b8f7c92046f869eea116f81a29a1f286489
Tsheri Sherpa <tsherisherpa@gmail.com>
2020-06-20 09:32:54 +0545

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = "tsherisherpa@gmail.com"
-app.config['MAIL_PASSWORD'] = "Gmail_@123"
+app.config['MAIL_PASSWORD'] = "gklhjklh"
 
 mail= Mail(app)
 
